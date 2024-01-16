5a3bc1b672bef6b6d4fd95c7893f22969ea41845
Fr3574 <frederik.stalschus@sap.com>
2024-01-16 13:07:47 +0100

    add vector matching

--- erpub/pipeline/matching.py
+++ erpub/pipeline/matching.py
@@ -1,4 +1,8 @@
-def jaccard_similarity(a: str, b: str) -> float:
+import numpy as np
+from scipy.spatial.distance import cosine
+
+
+def jaccard_similarity(a: str, b: str, embedding_table: None = None) -> float:
     """
     Calculate the average Jaccard Similarity across a given attribute.
     """
@@ -7,3 +11,12 @@ def jaccard_similarity(a: str, b: str) -> float:
     union = A.union(B)
     score = len(intersection) / len(union)
     return score
+
+
+def vector_embeddings(a: str, b: str, embedding_table: dict[str, np.ndarray]) -> float:
+    """Use word embeddings to map tokens to vectors,
+    average them and then return the cosine similarity"""
+    mean_vec_a = np.mean([embedding_table[token] for token in a.split()], axis=0)
+    mean_vec_b = np.mean([embedding_table[token] for token in b.split()], axis=0)
+    cos_sim = 1 - cosine(mean_vec_a, mean_vec_b)
+    return cos_sim
--- tests/test_matching.py
+++ tests/test_matching.py
@@ -1,6 +1,8 @@
 import pytest
 
-from erpub.pipeline.matching import jaccard_similarity
+import numpy as np
+
+from erpub.pipeline.matching import jaccard_similarity, vector_embeddings
 
 
 def test_jaccard_similarity():
@@ -11,3 +13,17 @@ def test_jaccard_similarity():
 def test_jaccard_similarity_fails_on_empty_attributes():
     with pytest.raises(ZeroDivisionError) as e:
         jaccard_similarity("", "")
+
+
+def test_vector_embeddings():
+    vec1, vec2 = np.random.rand(20,), np.random.rand(
+        20,
+    )
+    embedding_table = {
+        "foo": vec1,
+        "bar": vec2,
+        "Foo": vec1,
+    }
+    assert vector_embeddings("foo", "foo", embedding_table) == 1
+    assert vector_embeddings("foo", "Foo", embedding_table) == 1
+    assert vector_embeddings("Foo foo", "Foo bar", embedding_table) < 1
