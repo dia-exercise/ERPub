412a84b1798a1a578d9e4407c922ea42a6b37dc1
leshl <57219730+leshl@users.noreply.github.com>
2024-01-21 19:41:49 +0100

    Remove duplicates; add docstrings; always download to ERPub/erpub/data (#21)

--- erpub/data_preparation.py
+++ erpub/data_preparation.py
@@ -1,22 +1,29 @@
 import csv
+import os.path
 import tarfile
 import requests
 import logging
+import pandas as pd
 from pathlib import Path
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
 def download_input(url, target_dir):
-    Path(target_dir).mkdir(parents=True, exist_ok=True)
+    """
+    Download tarball from url in streaming mode and extract it in target_dir.
+    Skip download if tarball already exists in target_dir.
+    """
+    abs_target_dir = _get_abs_path(target_dir)
+    Path(abs_target_dir).mkdir(parents=True, exist_ok=True)
 
-    target_path = f"{target_dir}/{url.split('/')[-1]}"
+    target_path = f"{abs_target_dir}/{url.split('/')[-1]}"
     if Path(target_path).is_file():
         logging.info(f"File {target_path} already exists. Won't download again")
         return
 
     logging.info(f"Start downloading {url}")
-    # stream mode will first get the response header only
+    # streaming mode will first get the response header only
     response = requests.get(url, stream=True)
     with open(target_path, mode="wb") as file:
         # download file in chunks of 1MiB
@@ -29,33 +36,33 @@ def download_input(url, target_dir):
         file.extractall(target_dir)
 
 
-def set_dict_value(dict_: dict, key: str, value: str) -> None:
-    if dict_.get(key):
-        raise Exception(f"Key {key} already exists")
-    else:
-        dict_[key] = value
-
-
 def read_txt(input_file: str) -> list[dict]:
+    """
+    Read in txt file, formatted as described here: https://www.aminer.org/citation (v8).
+    Extract publication blocks. Filter out publications published before 1995 or after 2004. Filter out publications that
+    don't have 'sigmod' or 'vldb' in their publication venue name.
+    """
+    abs_input_path = _get_abs_path(input_file)
+
     current_block: dict[str, str] = {}
     filtered_blocks = []
 
-    logging.info(f"Start reading {input_file}")
-    with open(input_file, "r") as file:
+    logging.info(f"Reading {abs_input_path}")
+    with open(abs_input_path, "r") as file:
         # use set_dict_value() to populate dict. this is to ensure that the input file is properly formatted:
-        # if a block contains two lines of the same type (paper references are ignored), an exception will be thrown
+        # if a block contains two lines of the same type (except paper references), an exception will be thrown
         # if two blocks aren't separated by a newline, an exception will be thrown
         for line in file:
             if line.startswith("#*"):
-                set_dict_value(current_block, "paper_title", line[2:].rstrip())
+                _set_dict_value(current_block, "paper_title", line[2:].rstrip())
             elif line.startswith("#@"):
-                set_dict_value(current_block, "author_names", line[2:].rstrip())
+                _set_dict_value(current_block, "author_names", line[2:].rstrip())
             elif line.startswith("#t"):
-                set_dict_value(current_block, "year_of_publication", line[2:].rstrip())
+                _set_dict_value(current_block, "year_of_publication", line[2:].rstrip())
             elif line.startswith("#c"):
-                set_dict_value(current_block, "publication_venue", line[2:].rstrip())
+                _set_dict_value(current_block, "publication_venue", line[2:].rstrip())
             elif line.startswith("#index"):
-                set_dict_value(current_block, "paper_id", line[6:].rstrip())
+                _set_dict_value(current_block, "paper_id", line[6:].rstrip())
 
             elif line == "\n":
                 # exclude publications that don't provide the attributes we filter on
@@ -74,11 +81,23 @@ def read_txt(input_file: str) -> list[dict]:
     return filtered_blocks
 
 
-def write_csv(blocks: list[dict], target_dir: str, output_file: str) -> None:
-    Path(target_dir).mkdir(parents=True, exist_ok=True)
+def remove_duplicates(blocks: list[dict]) -> list[dict]:
+    """Remove duplicated publication blocks - i.e. all dict values are the same."""
 
-    target_path = f"{target_dir}/{output_file}"
-    logging.info(f"Write {target_path}")
+    df = pd.DataFrame(blocks)
+    df_dedup = df.drop_duplicates()
+    logging.info(f"Removed {df.shape[0] - df_dedup.shape[0]} duplicate publication entries")
+    return df_dedup.to_dict("records")
+
+
+def write_csv(blocks: list[dict], target_dir: str, output_file: str) -> None:
+    """Write publication blocks into csv file in target_dir."""
+
+    abs_target_dir = _get_abs_path(target_dir)
+    Path(abs_target_dir).mkdir(parents=True, exist_ok=True)
+
+    target_path = os.path.join(abs_target_dir, output_file)
+    logging.info(f"Writing {target_path}")
     with open(target_path, "w") as file:
         fields = ["paper_id", "paper_title", "author_names", "publication_venue", "year_of_publication"]
         writer = csv.DictWriter(file, fieldnames=fields)
@@ -86,20 +105,32 @@ def write_csv(blocks: list[dict], target_dir: str, output_file: str) -> None:
         writer.writerows(blocks)
 
 
+def _get_abs_path(rel_path: str) -> str:
+    """Turn rel_path into an absolute path, relative to this file."""
+    abs_file_dir = os.path.dirname(__file__)
+    return os.path.join(abs_file_dir, rel_path)
+
+
+def _set_dict_value(dict_: dict, key: str, value: str) -> None:
+    """Set key to value in dict if key does not yet exist. If it does already exist, throw exception."""
+    if dict_.get(key):
+        raise Exception(f"Key {key} already exists")
+    else:
+        dict_[key] = value
+
+
 if __name__ == "__main__":
     dblp_url = "https://lfs.aminer.cn/lab-datasets/citation/dblp.v8.tgz"
     acm_url = "https://lfs.aminer.cn/lab-datasets/citation/citation-acm-v8.txt.tgz"
-    # TODO where data is saved currently depends on the Python working directory
-    #  i.e. calling the script from the root will result in ERPub/data
-    #  calling it from erpub will result in ERPub/erpub/data
-    #  we can decide for a path and make this consistent
     download_target_dir = "data/raw"
     prepared_target_dir = "data/prepared"
 
     download_input(dblp_url, download_target_dir)
     dblp_blocks = read_txt(f"{download_target_dir}/dblp.txt")
+    dblp_blocks = remove_duplicates(dblp_blocks)
     write_csv(dblp_blocks, prepared_target_dir, "DBLP_1995_2004.csv")
 
     download_input(acm_url, download_target_dir)
     acm_blocks = read_txt(f"{download_target_dir}/citation-acm-v8.txt")
+    acm_blocks = remove_duplicates(acm_blocks)
     write_csv(acm_blocks, prepared_target_dir, "ACM_1995_2004.csv")
