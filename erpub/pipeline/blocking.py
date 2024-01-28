3455813df7ad53e6d7bd518ebd0f5690d942de34
Leon Schäffer <leonschaeffer@me.com>
2024-01-28 13:41:03 +0100

    Add blocking function for author name initials

--- erpub/pipeline/blocking.py
+++ erpub/pipeline/blocking.py
@@ -8,3 +8,29 @@ def naive_all_pairs(data: pd.DataFrame) -> None:
 
 def same_year_of_publication(data: pd.DataFrame) -> None:
     data["block"] = data.groupby("year_of_publication").ngroup()
+
+
+def author_names_initials(data: pd.DataFrame) -> None:
+    data["initials"] = data["author_names"]
+    data["initials"] = data["initials"].apply(_extract_initials)
+    data["block"] = data.groupby("initials").ngroup()
+    data.drop("initials", axis=1, inplace=True)
+
+
+def _extract_initials(names: str) -> str:
+    # assumption: names are separated by a comma
+    name_list = names.split(",")
+    name_list = [n.strip() for n in name_list]
+    # assumption: parts of names are separated by spaces
+    name_list = [n.split(" ") for n in name_list]
+    name_list = [[part.strip() for part in name_parts] for name_parts in name_list]
+    # get initials
+    name_list = [[part[0] for part in name_parts] for name_parts in name_list]
+    # only retain the initials of the first name and the last name; ignore middle names
+    name_list = [[initials[0], initials[-1]] for initials in name_list]
+    # join initials into a single word per name
+    name_list = ["".join(l) for l in name_list]
+    # sort initials to account for different name orders
+    name_list = sorted(name_list)
+    # separate initials of different authors by comma (no space)
+    return ",".join(name_list)
