0d9ca6a21063d9f8e8bd8af063fa14c9b7720498
leshl <57219730+leshl@users.noreply.github.com>
2024-01-29 22:29:15 +0100

    Add blocking function for author name initials (#25)
    * Add blocking function for author name initials
    
    * Disable type check for _extract_initials() because mypy is annoying

--- tests/test_blocking.py
+++ tests/test_blocking.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from erpub.pipeline.blocking import naive_all_pairs, same_year_of_publication
+from erpub.pipeline.blocking import naive_all_pairs, same_year_of_publication, author_names_initials
 
 
 @pytest.fixture
@@ -20,6 +20,18 @@ def sample_df():
                 2002,
                 2002,
             ],
+            "author_names": [
+                "Kenneth A. Ross",
+                "Kenneth A. Ross",
+                "Huai Yang, Li Xu, Wynne Hsu",
+                "Kenneth A. Ross",
+                "David W. Embley, Li Xu, Yihong Ding",
+                "C. J. Date",
+                "Yihong Ding, David W. Embley, Li Xu",
+                "Stephen Blott, Henry F.Korth",
+                "C. J. Date",
+                "C. J. Date",
+            ],
             "other_column": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         }
     )
@@ -35,3 +47,14 @@ def test_same_year_of_publication(sample_df):
     same_year_of_publication(sample_df)
     assert "block" in sample_df
     assert sample_df["block"].nunique() == 3
+
+
+def test_author_names_initials(sample_df):
+    author_names_initials(sample_df)
+    assert "block" in sample_df
+    assert sample_df["block"].nunique() == 5
+
+    values = sample_df["block"].values
+    assert values[0] == values[1] == values[3]
+    assert values[4] == values[6]
+    assert values[5] == values[8] == values[9]
